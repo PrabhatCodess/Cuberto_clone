705bf502e74f3ce13b3b5907d585648d2ebaec79
PrabhatCodess <ak1973dwivedi@gmail.com>
2024-04-13 22:15:10 +0530

    added all the required files

---
+++ script.js
@@ -0,0 +1,57 @@
+// mouse foll0wer //
+
+Shery.mouseFollower();
+
+// Magnet Effect //
+
+Shery.makeMagnet(".magnet");
+Shery.makeMagnet("#magnet");
+
+// MEdia circle Effect //
+Shery.hoverWithMediaCircle(".hvr", {
+    videos: ["./0.mp4", "./2.mp4", "./3.mp4"],
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+// Shery.imageEffect(".images", {
+//     style: 3,
+//     /*optional parameters
+//     these parameter dose not applies to custom scroll trigger callback */
+//     scrollSnapping: true,
+//     scrollSpeed: 6,
+//     touchSpeed: 6,
+//     damping: 7,
+//   });
